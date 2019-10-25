0353e6ebb5753e9334192e7dd7630b72fcf51b5c
South Matic <Southmatic729@gmail.com>
2019-10-25 01:16:06 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,24 +1,26 @@
+// =============================================================================
+// npm packages
+// =============================================================================
 require('dotenv').config()
 // var to require mysql package
-var mysql = require('mysql')
+const mysql = require('mysql')
 // var to require keys
-var keys = require('./keys.js')
+const keys = require('./keys.js')
 // var to require gradient string package
-var gradient = require('gradient-string')
+const gradient = require('gradient-string')
 // var to require file system package
-var fs = require('fs')
+const fs = require('fs')
 // var to require inquirer package
-var inquirer = require('inquirer')
+const inquirer = require('inquirer')
 // var to hold password
-var gateKey = keys.mysql_db
+const gateKey = keys.mysql_db
 // var to require table package
 const Table = require('cli-table')
-// var to configure table displayed
-
-// var to hold inventory
-var inventory = []
+// =============================================================================
+// Global Variables
+// =============================================================================
 // var to hold mysql config
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
 
   // Your port if not 3306
@@ -30,10 +32,16 @@ var connection = mysql.createConnection({
   password: gateKey.key,
   database: 'bamazon_db'
 })
-
+// var to hold inventory
+let inventory = []
+let inStock = []
+const display = '**************************************'
+const banner = '\n' + display + '  Welcome to Bamazon!!  ' + display
+const headerColor = 'yellow'
+const borderColor = 'green'
 connection.connect(function (err) {
   if (err) throw err
-  console.log('connected as id ' + connection.threadId + '\n')
+  console.log(gradient.vice('connected as id ' + connection.threadId + '\n'))
 })
 
 function logo () {
@@ -49,12 +57,11 @@ function logo () {
 
 function displayProducts () {
   var table = new Table({
-    style: { head: ['green'], border: ['blue'] },
+    style: { head: [headerColor], border: [borderColor] },
     head: ['Item Id', 'Name', 'Console', 'Price', 'Quantity'],
     colWidths: [9, 50, 15, 10, 10]
   })
   inventory = []
-  var display = '****************************************'
   console.log('Selecting all products...\n')
   connection.query('SELECT * FROM products', function (err, result) {
     if (err) throw err
@@ -63,18 +70,15 @@ function displayProducts () {
       table.push([result[i].item_id, result[i].product_name, result[i].department_name, result[i].price, result[i].stock_quantity])
       inventory.push(result[i].item_id)
     }
-    console.log('\n' + display + 'Welcome to Bamazon!!' + display)
+    console.log(gradient.vice(banner))
     console.log(table.toString())
-    console.log('Press any key to continue')
-
-    // connection.end()
+    console.log(gradient.vice('Press any key to continue'))
     ask()
   })
 }
 
 function updateProducts (product, newQuant, oldQuant) {
-  console.log('Processing purchase')
-  var query = connection.query('UPDATE products SET ? WHERE ?',
+  connection.query('UPDATE products SET ? WHERE ?',
     [
       {
         stock_quantity: newQuant
@@ -85,33 +89,34 @@ function updateProducts (product, newQuant, oldQuant) {
     ],
     function (error, newResult) {
       if (error) throw error
-      console.log(newResult.affectedRows + ' products updated!!\n')
-    })
-  console.log(query.sql)
-  displayPurchase(product, oldQuant)
+      console.log(gradient.vice(newResult.affectedRows + ' products updated!!\n'))
+      displayPurchase(product, oldQuant)
+    }
+  )
 }
 
 function displayPurchase (product, quantity) {
   var updatedTable = new Table({
-    style: { head: ['green'], border: ['blue'] },
+    style: { head: [headerColor], border: [borderColor] },
     head: ['Name', 'Console', 'Price', 'Quantity', 'Total'],
     colWidths: [50, 15, 10, 10, 15]
   })
   connection.query('SELECT * FROM products WHERE item_id=?', [product], function (error, result) {
     if (error) throw error
-    var cost = result[0].price * quantity
+    var cost = (result[0].price * quantity).toFixed(2)
     updatedTable.push([result[0].product_name, result[0].department_name, result[0].price, result[0].stock_quantity, cost])
-    console.log('Your purchase\n', updatedTable.toString(), '\nTotal purchase = $', cost)
+    console.log(gradient.vice('Your purchase'))
+    console.log(updatedTable.toString())
+    console.log(gradient.vice('Total purchase = $' + cost))
     nextGame()
   })
 }
 
 function purchasing (product, quant) {
-  console.log('product', product)
   connection.query('SELECT * FROM products WHERE item_id=?', [product], function (error, result) {
     if (error) throw error
     if (quant > result[0].stock_quantity) {
-      console.log('Insufficient Quantity!!')
+      console.log(gradient.summer('Insufficient Quantity!!'))
       nextGame()
     } else {
       var newQuant = result[0].stock_quantity - quant
@@ -139,21 +144,22 @@ function nextGame () {
     }
   })
 }
+
 // ask user what they want to do.
 function ask () {
   inquirer.prompt([
     {
       type: 'input',
-      message: 'What is the ITEM ID of the product you would like to buy?',
+      message: gradient.vice('What is the ITEM ID of the product you would like to buy?'),
       name: 'saleID',
       validate: function (value) {
-        // if user selects a non alphabet character or its already been guess, block input
+        // Force customer to enter only item ids
         return inventory.includes(parseFloat(value))
       }
     },
     {
       type: 'input',
-      message: 'How many units of the products would you like to buy?',
+      message: gradient.vice('How many units of the products would you like to buy?'),
       name: 'saleQuantity',
       validate: function (value) {
         var valid = !isNaN(parseFloat(value))
@@ -167,7 +173,8 @@ function ask () {
 }
 
 function start () {
-  logo()
+  // uncomment to see custom Logo
+  // logo()
   displayProducts()
 }
 // start app
